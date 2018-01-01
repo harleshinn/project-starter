6576abf93fb4826aef39f1c883984aff1bf74897
mily.perez <harleshinn@gmail.com>
2018-01-01 20:15:58 -0300

    First commit

---
+++ styleguide/components/Base/Headings.php
@@ -0,0 +1,10 @@
+<!-- components/Base/Headings.php -->
+<h1 class="title">Lorem ipsum dolor sit amet</h1>
+
+<h2 class="title">Lorem ipsum dolor sit amet</h2>
+
+<h3 class="title">Lorem ipsum dolor sit amet</h3>
+
+<h4 class="title">Lorem ipsum dolor sit amet</h4>
+
+<h5 class="title">Lorem ipsum dolor sit amet</h5>
